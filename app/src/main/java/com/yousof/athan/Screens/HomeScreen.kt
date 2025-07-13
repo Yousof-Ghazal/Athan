46ff6371ea6e8d4cc9699e7bfbcfa4a43cdc5f62
Yousof-Ghazal <yousofghazal@gmail.com>
2025-07-13 09:48:58 +0200

    added PrayerCards to the HomeScreen

--- app/src/main/java/com/yousof/athan/Screens/HomeScreen.kt
+++ app/src/main/java/com/yousof/athan/Screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.yousof.athan.Screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,13 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.yousof.athan.HomeScreenComponents.CurrentDate
+import com.yousof.athan.HomeScreenComponents.PrayerCard
 import com.yousof.athan.R
 import com.yousof.athan.ViewModel.PrayerViewModel
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 
@@ -32,10 +37,33 @@ viewModel: PrayerViewModel = PrayerViewModel()
             contentScale = ContentScale.Crop
         )
     }
-    Row(
-        modifier = Modifier.fillMaxSize().padding(30.dp)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ){
-       CurrentDate(uiState.value)
-    }
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ){
+            CurrentDate(uiState.value)
+        }
+        Row (
+            modifier = Modifier,
+          horizontalArrangement = Arrangement.spacedBy(36.dp)
 
+        ){
+            PrayerCard(title = "Fajr", data = uiState.value)
+            PrayerCard(title = "Dhuhr", data = uiState.value)
+            PrayerCard(title = "Sunrise", data = uiState.value)
+        }
+        Row (
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(36.dp)
+            ){
+            PrayerCard(title = "Asr", data = uiState.value)
+            PrayerCard(title = "Maghrib", data = uiState.value)
+            PrayerCard(title = "Isha", data = uiState.value)
+        }
+    }
 }
