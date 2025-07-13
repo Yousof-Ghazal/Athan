e23ae18e89fa4980f068b4d79356d6261cfec6a7
Yousof-Ghazal <yousofghazal@gmail.com>
2025-07-13 11:49:24 +0200

    Not Complete CurrentPrayerCountdown

--- app/src/main/java/com/yousof/athan/ui/theme/Color.kt
+++ app/src/main/java/com/yousof/athan/ui/theme/Color.kt
@@ -7,6 +7,6 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
-val Purple57 = Color(0xFF200A59)
+
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
